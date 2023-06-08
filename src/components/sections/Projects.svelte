7f32418d8e6d13c6e1a0c2d0357e775d4c373ffb
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2023-06-08 11:33:30 -0400

    images fixed

--- src/components/sections/Projects.svelte
+++ src/components/sections/Projects.svelte
@@ -4,42 +4,42 @@
 
 <div class="p-4 flex flex-wrap gap-6">
 	<Project
-		img="static/flikshop.png"
+		img="flikshop.png"
 		title="Flikshop LMS"
 		desc="Flikshop is a Learning Management System (LMS) made with Android Studio using Kotlin and XML."
 		view="/"
 		wip={false}
 	/>
 	<Project
-		img="static/this.png"
+		img="this.png"
 		title="This Website"
 		desc="This site was built using SvelteKit, TailwindCSS, TypeScript, Shadcn, and more."
 		view="/"
 		wip={false}
 	/>
 	<Project
-		img="static/pokedexsvelte.png"
+		img="pokedexsvelte.png"
 		title="SvelteKit Pokedex"
 		desc="This site was built using SvelteKit, TailwindCSS, TypeScript, PokeAPI, and more."
 		view="https://sveltekit-pokedex-taupe.vercel.app/"
 		wip={false}
 	/>
 	<Project
-		img="static/cz.png"
+		img="cz.png"
 		title="CZCreative"
 		desc="Built using SvelteKit, TailwindCSS, TypeScript, and more."
 		view="/"
 		wip={true}
 	/>
 	<Project
-		img="static/pokedexjs.png"
+		img="pokedexjs.png"
 		title="PokedexJS"
 		desc="Built with HTML, CSS, JavaScript, and PokeAPI."
 		view="https://jamesmontebell.github.io/PokeJsDex/"
 		wip={false}
 	/>
 	<Project
-		img="static/anime.png"
+		img="anime.png"
 		title="ANIME-DB"
 		desc="Built using SvelteKit, TailwindCSS, TypeScript, KitsuAPI, and more."
 		view="/"
